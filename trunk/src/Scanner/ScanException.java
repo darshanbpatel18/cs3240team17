d95d93d12fe30c751dc9013a0a595ed9f8638de3
AGWSpilot16@gmail.com <AGWSpilot16@gmail.com@03a2049a-a1c1-208f-8266-b20f49bfd53b>
2010-12-12 21:10:48 +0000

    Added ScanException.java it doesn't do anything special really, it just gives me an exception to throw you guys when things fail.

---
+++ trunk/src/Scanner/ScanException.java
@@ -0,0 +1,24 @@
+package Scanner;
+
+public class ScanException extends Exception {
+
+	public ScanException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ScanException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ScanException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ScanException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
